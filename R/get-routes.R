90e1f4dbdacc276b085aa08828111ea32729f6b2
mpadge <mark.padgham@email.com>
2016-08-05 17:17:05 +0200

    fix test

--- R/get-routes.R
+++ R/get-routes.R
@@ -1,7 +1,3 @@
-library (osmdatar)
-
-addTest (2, 4)
-
 get_routes <- function (bbox, from, to)
 {
     roads <- osmdatar::get_lines (bbox, key='highway')
---
+++ runTest.R
@@ -0,0 +1,20 @@
+#library (devtools)
+#library (Rcpp)
+
+#compile
+wd <- getwd ()
+while (length (grep ('osmprob', getwd ())) > 0) setwd ("..")
+#setwd ("~/master_thesis/osmprob/")
+#Rcpp::compileAttributes (".", verbose=TRUE)
+Rcpp::compileAttributes ("osmprob", verbose=TRUE)
+
+#test
+#setwd ("~/master_thesis/")
+
+devtools::document ('osmprob')
+devtools::load_all ("osmprob")
+#devtools::check ("osmprob")
+
+ls ("package:osmprob")
+?add_test
+add_test (1, 2)
