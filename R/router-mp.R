a024c0b97b8e433fc2f65d834658bbe0b9279f0d
Andreas Petutschnig <andreas@petutschnig.de>
2017-04-12 11:47:16 +0200

    fix assignment of probabilities

--- R/router-mp.R
+++ R/router-mp.R
@@ -48,6 +48,16 @@ osm_router <- function (netdf, start_node, end_node, eta=1)
 #' @param eta The parameter controlling the entropy (scale is arbitrary)
 #'
 #' @export
+#'
+#' @examples
+#' netdf <- data.frame (
+#'                      from_id = c (rep (0, 3), rep (1, 3), rep (2, 4),
+#'                               rep (3, 3), rep (4, 2), rep (5, 3)),
+#'                      to_id = c (1, 2, 5, 0, 2, 3, 0, 1, 3, 5,
+#'                               1, 2, 4, 3, 5, 0, 2, 4),
+#'                      d_weighted = c (7., 9., 14., 7., 10., 15., 9., 10.,11.,
+#'                               2., 15., 11., 6., 6., 9., 14., 2., 9.))
+#' prb <- getProbability (netdf, 1, 5)
 getProbability <- function (netdf, start_node, end_node, eta=1)
 {
     if (!(is (netdf, 'data.frame') | is (netdf, 'matrix')))
@@ -62,12 +72,20 @@ getProbability <- function (netdf, start_node, end_node, eta=1)
     netdf$xto <- as.numeric (as.character (netdf$xto))
     allids <- c (netdf$xfr, netdf$xto)
     allids <- unique (sort (allids))
-    netdf$xfr <- vapply (netdf$xfr, function (x) {which (allids == x) }, 0.)
-    netdf$xto <- vapply (netdf$xto, function (x) {which (allids == x) }, 0.)
+    netdf$xfr <- vapply (netdf$xfr, function (x) { which (allids == x) -1 }, 0.)
+    netdf$xto <- vapply (netdf$xto, function (x) { which (allids == x) -1 }, 0.)
 
     eta <- as.numeric (eta * nrow (netdf))
     prob <- rcpp_router_prob (netdf, start_node, end_node, eta)
-    probability <- prob [prob != 0]
-    probability <- probability [-1]
-    prb <- cbind (netdf_out, probability)   
+
+    len <- dim (netdf) [1]
+    probability <- vector (length = len)
+    for (i in seq_len (len))
+    {
+        ln <- netdf [i, ]
+        probability [i] <- prob [ln$xto + 1, ln$xfr + 1]
+    }
+
+    prb <- cbind (netdf_out, probability)
+    prb
 }
