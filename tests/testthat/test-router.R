4a3b53661544825dd67394b739ffa76ec4bba36c
Andreas Petutschnig <andreas@petutschnig.de>
2017-04-12 11:51:31 +0200

    add unit test for getProbability ()

--- tests/testthat/test-router.R
+++ tests/testthat/test-router.R
@@ -9,3 +9,16 @@ test_that ("osm_router", {
     way <- osm_router (netdf, 0, 5, eta = 1.0)
     testthat::expect_is (way, "matrix")
 })
+
+test_that ("getProbability", {
+   netdf <- data.frame (
+                        'from_id' = c (rep (0, 3), rep (1, 3), rep (2, 4),
+                                   rep (3, 3), rep (4, 2), rep (5, 3)),
+                        'to_id' = c (1, 2, 5, 0, 2, 3, 0, 1, 3, 5,
+                                   1, 2, 4, 3, 5, 0, 2, 4),
+                        'd_weighted' = c (7., 9., 14., 7., 10., 15., 9., 10.,
+                                          11., 2., 15., 11., 6., 6., 9., 14.,
+                                          2., 9.))
+    way <- getProbability (netdf, 0, 5, eta = 1.0)
+    testthat::expect_is (way, "data.frame")
+})
